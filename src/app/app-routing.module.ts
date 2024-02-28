f33386e7a906fad520e4dd05502fc420be171d0a
diegofceliv <fernando.dfcv@gmail.com>
2024-02-27 23:20:02 -0500

    feat: udistrital/sga_documentacion#83 Se crea proyecto micro-front vacio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { APP_BASE_HREF } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { EmptyRouteComponent } from "./empty-route/empty-route.component";
+
+export const routes: Routes = [
+  {
+    path: 'empty-route',
+    component: EmptyRouteComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/notas/" }],
+})
+export class AppRoutingModule {}
